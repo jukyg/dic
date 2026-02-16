a6bca0d9154ee46ed8f6019ef77d7fa7ba0957a2
jukyg <safi2423f@gmail.com>
2026-02-16 23:11:27 +0300

    Delete .github/workflows directory

--- .github/workflows/index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js-selfbot-v13');
-const client = new Discord.Client({ checkUpdate: false });
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({ status: 'online' });
-});
-
-client.login(process.env.TOKEN);
